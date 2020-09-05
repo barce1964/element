1ebc10959d737f9fc7a5862af2752ac3b6ca2f2e
Alexander <alexvictar@mail.ru>
2020-09-05 23:27:12 +0600

    get elements

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+var box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart');
+
+console.log(box);
+console.log(btn);
+console.log(btn[0]);
+console.log(circle);
+console.log(circle[0]);
+console.log(heart[2]);
+console.log(oneHeart);
