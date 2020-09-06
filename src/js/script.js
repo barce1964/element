d15025b8d6495003485f2dc20f8d163e749e9e91
Alexander <alexvictar@mail.ru>
2020-09-06 16:56:29 +0600

    work with any elements

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,36 @@ var box = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByClassName('circle'),
     heart = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('.heart');
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
 
-console.log(box);
-console.log(btn);
-console.log(btn[0]);
-console.log(circle);
-console.log(circle[0]);
-console.log(heart[2]);
-console.log(oneHeart);
+box.style.backgroundColor = 'blue';
+btn[1].style.borderRadius = '40%';
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
+
+// for (var i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = 'blue';
+// }
+
+// heart.forEach(function(item, i, hearts){
+//     item.style.backgroundColor = 'blue';
+// });
+
+var div = document.createElement('div'),
+    text = document.createTextNode('Тут был я.');
+
+div.classList.add('black');
+
+// div.innerHTML = '<h1>Hello world!</h1>';
+div.textContent = 'Hello world!';
+//document.body.appendChild(div);
+// wrapper.appendChild(div);
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+wrapper.removeChild(heart[1]);
+document.body.replaceChild(heart[1], circle[0]);
+document.body.replaceChild(btn[1], heart[1]);
+
+console.log(div);
